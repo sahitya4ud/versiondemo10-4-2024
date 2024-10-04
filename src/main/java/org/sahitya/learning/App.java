7066317a0dbd440942b3b3fa9ce10077e4b8e713
sahitya4ud <sahitya4ud@gmail.com>
2024-10-04 10:27:15 -0500

    learning git

---
+++ src/main/java/org/sahitya/learning/App.java
@@ -0,0 +1,13 @@
+package org.sahitya.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
